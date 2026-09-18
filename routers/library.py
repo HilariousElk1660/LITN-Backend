6b254564d42661a7d9d90135f865322099f120ed
Divine-Ntambwe <divinentambwe28@gmail.com>
2026-09-18 09:29:52 +0200

    finished offline books

--- routers/library.py
+++ routers/library.py
@@ -12,27 +12,29 @@ async def get_my_library(current_user: dict = Depends(get_current_user)):
     async with get_connection() as conn:
         rows = await conn.fetch(
             """
-            SELECT
-                rb.reader_book_id,
-                rb.book_id,
-                b.book_name,
-                b.author_name,
-                b.book_cover_url,
-                rb.current_page,
-                rb.total_pages,
-                rb.current_chapter_index,
-                b.chapters AS total_chapters,
-                rb.percentage_completed,
-                rb.progress,
-                rb.last_opened,
-                rp.page_stopped_at,
-                rp.last_opened_on
-            FROM readers_books rb
-            JOIN books b ON b.book_id = rb.book_id
-            LEFT JOIN reading_progress rp
-                ON rp.book_id = rb.book_id AND rp.reader_id = rb.user_id
-            WHERE rb.user_id = $1
-            ORDER BY rb.last_opened DESC NULLS LAST
+          SELECT
+            rb.reader_book_id,
+            rb.book_id,
+            b.book_name,
+            b.author_name,
+            b.book_cover_url,
+            bf.pdf_file_url,
+            rb.current_page,
+            rb.total_pages,
+            rb.current_chapter_index,
+            b.chapters AS total_chapters,
+            rb.percentage_completed,
+            rb.progress,
+            rb.last_opened,
+            rp.page_stopped_at,
+            rp.last_opened_on
+        FROM readers_books rb
+        JOIN books b ON b.book_id = rb.book_id
+        LEFT JOIN book_files bf ON bf.book_id = b.book_id
+        LEFT JOIN reading_progress rp
+            ON rp.book_id = rb.book_id AND rp.reader_id = rb.user_id
+        WHERE rb.user_id = $1
+        ORDER BY rb.last_opened DESC NULLS LAST
             """,
             current_user["user_id"],
         )
